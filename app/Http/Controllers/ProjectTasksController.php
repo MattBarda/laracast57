06e0f7ffe14d58ead71ae4a3680e0a2c1a110552
matthew <matthew.barda@neverwhatif.com>
2019-01-16 10:09:20 +0000

    More controllers approach.

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -8,24 +8,6 @@ use App\Task;
 
 class ProjectTasksController extends Controller
 {
-    public function update(Task $task, Request $request)
-    {
-        //Different ways of doing things 
-
-//        if($request->has('completed')){
-//            $task->complete();
-//        }else{
-//            $task->incomplete();
-//        }
-//
-//        $request->has('completed') ? $task->complete() : $task->incomplete();
-
-        $method = $request->has('completed') ? 'complete' : 'incomplete';
-        $task->$method;
-
-        return back();
-    }
-
     public function store(Project $project, Request $request)
     {
         $attributes = $request->validate(['description' => 'required']);
--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -11,8 +11,10 @@
         <div class="box">
             @foreach($project->tasks as $task)
                 <div>
-                    <form method="POST" action="/tasks/{{ $task->id  }}">
-                        @method('PATCH')
+                    <form method="POST" action="/completed-tasks/{{ $task->id  }}">
+                        @if($task->completed)
+                            @method('DELETE')
+                        @endif
                         @csrf
                         <label for="completed" class="checkbox">
                             <input type="checkbox" name="completed" onchange="this.form.submit()" {{ $task->completed ? 'checked' : '' }}>
