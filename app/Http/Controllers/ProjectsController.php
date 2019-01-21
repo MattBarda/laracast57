717bf49ebad13199ac3987bab6c448b163359799
matthew <matthew.barda@neverwhatif.com>
2019-01-21 12:58:40 +0000

    Use gates for authorization, and different ways of authorizing.

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -6,11 +6,13 @@ use App\Project;
 use App\Services\Twitter;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class ProjectsController extends Controller
 {
     public function __construct()
     {
+        $this->middleware('can:update,project')->only(['show', 'update', 'destroy', 'edit', 'index']);
         $this->middleware('auth');
 //        $this->middleware('auth')->only(['show']); apply only to show route
     }
@@ -30,6 +32,17 @@ class ProjectsController extends Controller
     {
         //dump Twitter service
 //        dd($twitter);
+
+        //Different authorization methods
+//        abort_unless(Auth::user()->owns($project), 403);
+//        abort_if($project->id !== Auth::id(), 403);
+//        if ( Gate::denies('update', $project)) abort(403);
+//        abort_if(Gate::denies('update', $project), 403);
+//        abort_unless(Gate::allows('update', $project), 403);
+//        Auth::user()->can('update', $project);
+
+        $this->authorize('update', $project);
+
         return view('projects.show', compact('project'));
     }
 
@@ -44,9 +57,11 @@ class ProjectsController extends Controller
             'title' => ['required', 'min:3'],
             'description' => ['required', 'min:3'],
         ]);
+
         // request('title') - other way of accessing request attributes
         // request(['title', 'description']); - mass accessing request attributes
         $attributes['owner_id'] = auth()->id();
+
         Project::create($attributes);
 
         return redirect('/projects');
